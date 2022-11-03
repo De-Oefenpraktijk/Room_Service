88405a40235cc56bd84dc747b87e750723351403
Rense <rensevanstiphout@gmail.com>
2022-11-03 10:24:20 +0100

    api versioning

--- Room_Service/Controllers/WorkspaceController.cs
+++ Room_Service/Controllers/WorkspaceController.cs
@@ -12,7 +12,7 @@ using Room_Service.Entities;
 
 namespace Room_Service.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/v1/[controller]")]
     [ApiController]
     public class WorkspaceController : Controller
     {
