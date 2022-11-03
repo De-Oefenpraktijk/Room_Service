57ea0fa10a071e0a63d1005dd71155d651d8c932
Rense <rensevanstiphout@gmail.com>
2022-11-03 10:14:22 +0100

    Nex unit and integration

---
+++ Room_Service.XIntegration/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Room_Service.XIntegration;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ Room_Service.XUnit/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Room_Service.XUnit;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
