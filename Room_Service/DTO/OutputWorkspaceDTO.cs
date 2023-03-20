16e675a1e06e4516b42dff8bb1c342a02274a2dc
Antonio Takev <tonitakev@gmail.com>
2023-03-20 11:23:58 +0100

    Refactor of DTOs
    Faciliate transferring data by making two types of DTOs - Input and Output.
    
    Refs: Jira Issue OP-58

---
+++ Room_Service/DTO/OutputWorkspaceDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using Room_Service.Entities;
+using System.Text.Json.Serialization;
+using System.ComponentModel.DataAnnotations;
+
+namespace Room_Service.DTO
+{
+    public class OutputWorkspaceDTO
+    {
+        public string? id { get; set; }
+        [Required]
+        public string name { get; set; }
+
+        public string? imageFile { get; set; }
+
+        public List<FileDTO>? files { get; set; }
+
+        public List<OutputRoomDTO>? rooms { get; set; }
+    }
+}
+
