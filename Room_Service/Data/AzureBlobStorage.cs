3c58ae133e485da995023f84a23d612e58683a07
Stelian Rumenov <stelian@duodeka.nl>
2023-04-03 14:48:05 +0200

    removed memes

---
+++ Room_Service/Data/AzureBlobStorage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Room_Service.Data
+{
+	public class AzureBlobStorage
+	{
+		public AzureBlobStorage()
+		{
+            const string AccountName = "oefenpraktijkstorageacc";
+            const string AccountKey = "wOzd9fNz/IKcurS7vkv49WJ9wYTe8Y7avYYcvfFrAt04GpSmO/Y8kb82UeLjau0El1y5txLUoj75+AStcMviFg==";
+            const string ContainerName = "workspace-images";
+        }
+	}
+}
+
